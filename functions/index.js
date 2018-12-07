64cf4568e4e8cbbe64731468f335ba239c1906e1
Fabien Thiombiano <thiombianofabien@gmail.com>
2018-12-06 19:49:25 -0600

    https file upload trigger function

--- functions/index.js
+++ functions/index.js
@@ -2,11 +2,14 @@ const os = require('os');
 const path = require('path');
 const functions = require('firebase-functions');
 const { Storage } = require('@google-cloud/storage');
+const cors = require('cors')({ origin: true});
+const Busboy = require('busboy');
+const fs = require('fs');
 
 // config
 const gcs = new Storage({
-     projectId: '', 
-     keyFilename: ''
+     projectId: 'cloud-functions-basics', 
+     keyFilename: 'cloud-functions-basics-firebase-adminsdk-zafvq-716fab6d49.json'
 });
 
 // // Create and Deploy Your First Cloud Functions
@@ -16,7 +19,7 @@ const gcs = new Storage({
 //  response.send("Hello from Firebase!");
 // });
 
-
+// storage trigger
 exports.onImageUpload = functions.storage.bucket("cloud-functions-basics.appspot.com").object().onFinalize(event => {
     console.log(event);
 
@@ -48,3 +51,46 @@ exports.onImageDeletion = functions.storage.bucket("cloud-functions-basics.appsp
     console.log(event);
 });
 
+// https trigger
+exports.uploadFile = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+      if (req.method !== "POST") {
+        return res.status(500).json({
+          message: "Request is not allowed!"
+        });
+      }
+      const busboy = new Busboy({ headers: req.headers });
+      let uploadData = null;
+  
+      busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+        const filepath = path.join(os.tmpdir(), filename);
+        uploadData = { file: filepath, type: mimetype };
+        file.pipe(fs.createWriteStream(filepath));
+      });
+  
+      busboy.on("finish", () => {
+        const bucket = gcs.bucket("cloud-functions-basics.appspot.com");
+        bucket
+          .upload(uploadData.file, {
+            uploadType: "media",
+            metadata: {
+              metadata: {
+                contentType: uploadData.type
+              }
+            }
+          })
+          .then(() => {
+            res.status(200).json({
+              message: "Upload was successfull!"
+            });
+          })
+          .catch(err => {
+            res.status(500).json({
+              error: err
+            });
+          });
+      });
+      
+      busboy.end(req.rawBody);
+    });
+  });
